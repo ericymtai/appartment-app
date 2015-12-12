941403c15895fdda8ada1337f187e47d9ea35673
ericytai <ericym2@gmail.com>
2015-12-12 13:20:43 -0500

    first commit

---
+++ Eric_Tai_final_assignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eric_Tai_final_assignment
+//
+//  Created by Eric Tai on 2015-12-08.
+//  Copyright © 2015 Eric Tai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
